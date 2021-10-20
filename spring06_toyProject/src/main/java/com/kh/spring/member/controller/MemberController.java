ac71e057441584d938acbb27c0910acc42ff86ef
kyungmin <alssgo70051@naver.com>
2021-10-20 12:02:52 +0900

    [10/20] @CookieValue @SessionAttribute

--- spring06_toyProject/src/main/java/com/kh/spring/member/controller/MemberController.java
+++ spring06_toyProject/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.kh.spring.member.model.dto.Member;
 import com.kh.spring.member.model.service.MemberService;
@@ -77,7 +79,9 @@ public class MemberController {
 	}
 	
 	@GetMapping("mypage")
-	public void mypage() {
-		
+	public void mypage(@CookieValue(name = "JSESSIONID") String sessionId
+					, @SessionAttribute(name = "authentication") Member member) {
+		logger.debug("@CookieValue : " + sessionId);
+		logger.debug("@SessionAttribute : " + member);
 	}
 }
